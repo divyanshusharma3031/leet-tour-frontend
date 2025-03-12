3d368166b4a976a997db070d10877464b31cd64c
divyanshusharma3031 <divyanshukatyan_co20b3_55@dtu.ac.in>
2025-03-12 14:30:14 +0530

    first commit- initial setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  return (
+    <div
+      className="container min-h-screen pt-5 bg-cover bg-center bg-no-repeat bg-[url('/bg-3.jpg')]"
+      
+    >
+      {/* <div className="absolute inset-0 bg-black/10 backdrop-blur-md"></div> */}
+      <div className="relative z-10 container w-[1170px] m-auto">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold text-[#fff]">leetForces</h1>
+          <div className="flex font-light text-[16px] m-[8px]">
+            <div className="">
+              <Link href="/login">
+                <span className="pt-[8px] pr-[15px] pb-[10px] pl-[15px] text-[#fff]">
+                  Login
+                </span>
+              </Link>
+            </div>
+            <div className="">
+              <Link href="/login">
+                <span className="pt-[8px] pr-[15px] pb-[10px] pl-[15px] text-[#fff]">
+                  Signup
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
